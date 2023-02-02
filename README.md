ff5700a8931cccf289e0a958b1cef0683699fe98
maheshbaaru <123924054+maheshbaaru@users.noreply.github.com>
2023-02-02 14:47:43 +0530

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# angular-table-add-delete-row-k7zw3t
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/angular-table-add-delete-row-k7zw3t)
---
+++ src/app/table/table.component.ts
@@ -0,0 +1,74 @@
+import { HttpClient } from '@angular/common/http';
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-table',
+  templateUrl: './table.component.html',
+  styleUrls: ['./table.component.css'],
+})
+export class TableComponent {
+  total = 0;
+
+  row = [
+    {
+      id: '',
+      name: '',
+      price: '',
+      quantity: '',
+      total: '',
+    },
+    {
+      id: '',
+      name: '',
+      price: '',
+      quantity: '',
+      total: '',
+    },
+    {
+      id: '',
+      name: '',
+      price: '',
+      quantity: '',
+      total: '',
+    },
+  ];
+  constructor() {}
+
+  ngOnInit() {
+    this.row[0].id = '1';
+    this.row[1].id = '2';
+    this.row[2].id = '3';
+  }
+
+  addTable() {
+    console.log(this.row.length);
+    const obj = {
+      id: '',
+      name: '',
+      price: '',
+      quantity: '',
+      total: '',
+    };
+
+    this.row.push(obj);
+    this.row[this.row.length - 1].id = this.row.length.toString();
+  }
+
+  deleteRow(x) {
+    var delBtn = confirm(' Do you want to delete ?');
+    if (delBtn == true) {
+      this.row.splice(x, 1);
+    }
+  }
+
+  // addOrder() {
+  //   const resp = this.http.post(this.url, this.row).subscribe(
+  //     (response) => {
+  //       console.log(response);
+  //     },
+  //     (response) => {
+  //       console.log(response);
+  //     }
+  //   );
+  // }
+}
