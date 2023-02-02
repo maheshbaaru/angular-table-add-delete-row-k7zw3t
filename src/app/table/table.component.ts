f9d34f47fc3bdaead89c9cdcd998b8792e37aba2
maheshbaaru <123924054+maheshbaaru@users.noreply.github.com>
2023-02-02 18:01:55 +0530

    2 files modified

--- src/app/table/table.component.ts
+++ src/app/table/table.component.ts
@@ -7,57 +7,88 @@ import { Component } from '@angular/core';
   styleUrls: ['./table.component.css'],
 })
 export class TableComponent {
-  total = 0;
-
+  // total = 0;
+  // row = [
+  //   {
+  //     id: '',
+  //     name: '',
+  //     price: '',
+  //     quantity: '',
+  //     total: '',
+  //   },
+  //   {
+  //     id: '',
+  //     name: '',
+  //     price: '',
+  //     quantity: '',
+  //   },
+  //   {
+  //     id: '',
+  //     name: '',
+  //     price: '',
+  //     quantity: '',
+  //   },
+  // ];
+  // constructor() {}
+  // ngOnInit() {
+  //   this.row[0].id = '1';
+  //   this.row[1].id = '2';
+  //   this.row[2].id = '3';
+  // }
+  // addTable() {
+  //   console.log(this.row.length);
+  //   const obj = {
+  //     id: '',
+  //     name: '',
+  //     price: '',
+  //     quantity: '',
+  //   };
+  //   this.row.push(obj);
+  //   this.row[this.row.length - 1].id = this.row.length.toString();
+  // }
+  // deleteRow(x) {
+  //   // var delBtn = confirm(' Do you want to delete ?');
+  //   // if (delBtn == true) {
+  //   //   this.row.splice(x, 1);
+  //   // }
+  //   this.row.splice(x, 1);
+  // }
   row = [
     {
       id: '',
       name: '',
-      price: '',
-      quantity: '',
-      total: '',
+      email: '',
     },
     {
       id: '',
       name: '',
-      price: '',
-      quantity: '',
-      total: '',
+      email: '',
     },
     {
       id: '',
       name: '',
-      price: '',
-      quantity: '',
-      total: '',
+      email: '',
     },
   ];
-  constructor() {}
-
-  ngOnInit() {
-    this.row[0].id = '1';
-    this.row[1].id = '2';
-    this.row[2].id = '3';
-  }
 
   addTable() {
-    console.log(this.row.length);
     const obj = {
       id: '',
       name: '',
-      price: '',
-      quantity: '',
-      total: '',
+      email: '',
     };
-
     this.row.push(obj);
-    this.row[this.row.length - 1].id = this.row.length.toString();
   }
 
   deleteRow(x) {
-    var delBtn = confirm(' Do you want to delete ?');
-    if (delBtn == true) {
-      this.row.splice(x, 1);
-    }
+    // var delBtn = confirm(' Do you want to delete ?');
+    // if (delBtn == true) {
+    //   this.row.splice(x, 1);
+    // }
+    this.row.splice(x, 1);
+  }
+
+  addrow(x) {
+    this.row.splice(x + 1, 0, x);
   }
 }
